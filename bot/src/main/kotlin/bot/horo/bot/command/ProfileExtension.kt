cf1caadca4c9b4c19eb9b7b40d2c9fd07c3764d9
Giang <463189@student.fontys.nl>
2022-12-08 00:46:50 +0100

    Make profile prettier

--- bot/src/main/kotlin/bot/horo/bot/command/ProfileExtension.kt
+++ bot/src/main/kotlin/bot/horo/bot/command/ProfileExtension.kt
@@ -1,36 +1,38 @@
 package bot.horo.bot.command
 
 import com.kotlindiscord.kord.extensions.commands.Arguments
-import com.kotlindiscord.kord.extensions.commands.converters.impl.snowflake
+import com.kotlindiscord.kord.extensions.commands.converters.impl.user
 import com.kotlindiscord.kord.extensions.extensions.Extension
-import com.kotlindiscord.kord.extensions.extensions.ephemeralSlashCommand
+import com.kotlindiscord.kord.extensions.extensions.publicSlashCommand
 import com.kotlindiscord.kord.extensions.types.respond
 import com.kotlindiscord.kord.extensions.utils.createdAt
+import com.kotlindiscord.kord.extensions.utils.profileLink
 import dev.kord.common.Color
 import dev.kord.rest.builder.message.create.embed
 
 import java.text.DateFormat
 import java.text.SimpleDateFormat
+import java.time.format.DateTimeFormatter
 
 class ProfileExtension : Extension() {
     override val name: String = "profile"
     override val bundle: String = "Display a users profile"
 
     private class ProfileArguments : Arguments() {
-         val id by snowflake {
-            name = "id"
-            description = "put a user id here"
+         val target by user {
+            name = "tag"
+            description = "tag a user"
         }
     }
 
     override suspend fun setup() {
-        ephemeralSlashCommand({ProfileArguments()}) { //pass an instance of the ProfileArguments class as an argument
+        publicSlashCommand(::ProfileArguments) { //pass an instance of the ProfileArguments class as an argument
             name = "profile"
             description = "Display a users profile"
 
             action {
                 //access the ID from the parsed arguments
-                val id =  arguments.id
+                val id =  arguments.target.id
 
                 //use the user ID to get the user from the guild
                 val guild = this@action.guild!!
@@ -40,32 +42,39 @@ class ProfileExtension : Extension() {
                 fun formatDate(rawDate: String): String {
                     val inputFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
                     val parsedDate = inputFormat.parse(rawDate)
-                    val dateFormat = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT)
+                    val dateFormat = DateFormat.getDateTimeInstance(DateFormat.FULL, DateFormat.SHORT)
                     return dateFormat.format(parsedDate)
                 }
                 respond {
-                    content = "content"
                     embed {
-                        title = "Profile"
-                        field {
-                            name = "ID"
-                            value = user.id.toString()
+                        author {
+                            icon = user.avatar?.url.toString()
+                            name = user.tag
                         }
                         field {
                             name = "Display name"
-                            value = user.displayName
+                            value = user.mention
                         }
                         field {
-                            name = "User name"
-                            value = user.username + "#" + user.discriminator
-                        }
-                        field {
-                            name = "Account created At"
+                            name = "Created at"
                             value = formatDate(user.createdAt.toString())
                         }
                         field {
-                            name = "Server Joined at"
+                            name = "Joined at"
                             value = formatDate(user.joinedAt.toString())
+                        }
+                        field {
+                            name = "Roles: " + user.roleIds.size.toString()
+                            value = user.roleIds.joinToString { "<@&$it>" }
+                        }
+                        field {
+                            name = "Profile link"
+                            value = user.profileLink
+                        }
+                        footer {
+                            text = "ID:" + user.id.toString() + " ┇ " + java.time.LocalDateTime.now().format(
+                                DateTimeFormatter.ofPattern("E d MMM uuuu")) + " at " +  java.time.LocalDateTime.now().format(
+                                DateTimeFormatter.ofPattern("HH:mm"))
                         }
                         color = Color(202, 117, 201)
                         thumbnail {
