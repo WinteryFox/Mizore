f4d81c163248e7cda714c4836ac0316221ab8db6
Giang <463189@student.fontys.nl>
2022-12-12 00:52:43 +0100

    Add subcommands for avatar and display profile

--- bot/src/main/kotlin/bot/horo/bot/command/ProfileExtension.kt
+++ bot/src/main/kotlin/bot/horo/bot/command/ProfileExtension.kt
@@ -1,6 +1,7 @@
 package bot.horo.bot.command
 
 import com.kotlindiscord.kord.extensions.commands.Arguments
+import com.kotlindiscord.kord.extensions.commands.application.slash.publicSubCommand
 import com.kotlindiscord.kord.extensions.commands.converters.impl.user
 import com.kotlindiscord.kord.extensions.extensions.Extension
 import com.kotlindiscord.kord.extensions.extensions.publicSlashCommand
@@ -8,73 +9,98 @@ import com.kotlindiscord.kord.extensions.types.respond
 import com.kotlindiscord.kord.extensions.utils.createdAt
 import dev.kord.common.Color
 import dev.kord.common.toMessageFormat
+import dev.kord.rest.builder.message.create.actionRow
 import dev.kord.rest.builder.message.create.embed
 import kotlinx.datetime.Clock
-import kotlinx.datetime.Instant
 
 class ProfileExtension : Extension() {
-    override val name: String = "profile"
-    override val bundle: String = "Display a users profile"
+    override val name: String = "horo.profile"
+    override val bundle: String = "horo.profile"
 
     private class ProfileArguments : Arguments() {
-         val target by user {
+        val target by user {
             name = "tag"
             description = "tag a user"
         }
     }
 
     override suspend fun setup() {
-        publicSlashCommand(::ProfileArguments) { //pass an instance of the ProfileArguments class as an argument
+        publicSlashCommand { //pass an instance of the ProfileArguments class as an argument
             name = "profile"
-            description = "Display a users profile"
+            description = "profile commands"
+            val theme = Color(202, 117, 201)
 
-            action {
-                //access the ID from the parsed arguments
-                val id =  arguments.target.id
+            publicSubCommand(::ProfileArguments)
+            {
+                name = "avatar"
+                description = "Display a users avatar"
 
-                //use the user ID to get the user from the guild
-                val guild = this@action.guild!!
-                val user = guild.getMember(id)
+                action {
+                    //access the ID from the parsed arguments
+                    val id = arguments.target.id
 
-                val joinedAt = user.joinedAt
-                val createdAt = user.createdAt
-                val now = Clock.System.now()
-                val joinedDate = joinedAt.toMessageFormat()
-                val createdDate = createdAt.toMessageFormat()
+                    //use the user ID to get the user from the guild
+                    val guild = this@action.guild!!
+                    val user = guild.getMember(id)
 
-                respond {
-                    embed {
-                        author {
-                            icon = user.avatar?.url.toString()
-                            name = user.tag
+                    val imageUrl = user.avatar?.url.toString() + "?size=512"
+
+                    respond {
+                        embed {
+                            image = imageUrl
+                            color = theme
                         }
-                        field {
-                            name = "Nickname"
-                            value = user.mention
+                        actionRow {
+                            linkButton(imageUrl) {
+                                label = "Open source"
+                            }
                         }
-                        field {
-                            name = "Joined at"
-                            value = joinedDate
-                        }
-                        field {
-                            name = "Roles (" + user.roleIds.size.toString() + ")"
-                            value = user.roleIds.joinToString { "<@&$it>" }
-                        }
-                        field {
-                            name = "Created at"
-                            value = createdDate
-                        }
-                        field {
-                            name = "Avatar source"
-                            value = user.avatar?.url.toString()
-                        }
-                        footer {
-                            text = "ID:" + user.id.toString()
-                        }
-                        timestamp = now
-                        color = Color(202, 117, 201)
-                        thumbnail {
-                            url = user.avatar?.url.toString()
+                    }
+                }
+            }
+
+            publicSubCommand(::ProfileArguments) {
+                name = "view"
+                description = "Display a users profile"
+
+                action {
+                    //access the ID from the parsed arguments
+                    val id = arguments.target.id
+
+                    //use the user ID to get the user from the guild
+                    val guild = this@action.guild!!
+                    val user = guild.getMember(id)
+
+                    respond {
+                        embed {
+                            author {
+                                icon = user.avatar?.url.toString()
+                                name = user.tag
+                            }
+                            field {
+                                name = "Nickname"
+                                value = user.mention
+                            }
+                            field {
+                                name = "Joined at"
+                                value = user.joinedAt.toMessageFormat()
+                            }
+                            field {
+                                name = "Roles (" + user.roleIds.size.toString() + ")"
+                                value = user.roleIds.joinToString { "<@&$it>" }
+                            }
+                            field {
+                                name = "Created at"
+                                value = user.createdAt.toMessageFormat()
+                            }
+                            footer {
+                                text = "ID:" + user.id.toString()
+                            }
+                            timestamp = Clock.System.now()
+                            color = theme
+                            thumbnail {
+                                url = user.avatar?.url.toString()
+                            }
                         }
                     }
                 }
